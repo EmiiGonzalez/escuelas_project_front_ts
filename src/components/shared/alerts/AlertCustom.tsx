59fa2f54240196a9e4df9c960179fa5fccb3ed5a
Emiliano Gonzalez <emigonzalez3333@gmail.com>
2024-10-31 11:21:41 -0300

    fix: :bug: Fix position and styles

--- src/components/shared/alerts/AlertCustom.tsx
+++ src/components/shared/alerts/AlertCustom.tsx
@@ -1,11 +1,11 @@
-import { Alert, Snackbar } from "@mui/material";
+import { Alert, Slide, SlideProps, Snackbar } from "@mui/material";
 import { AlertCustomProps } from "./AlertCustomProps";
-import { useEffect, useState } from "react";
-import { useThemeStore } from "../../../util/context/useThemeStore";
 
+const Transition = (props: SlideProps) => {
+  return <Slide {...props} direction="down" />;
+};
 export const AlertCustom = (props: AlertCustomProps) => {
   const { openToast, setOpenToast, variante, msg } = props;
-  const { tema } = useThemeStore();
   const handleClose = (
     _event?: React.SyntheticEvent | Event,
     reason?: string
@@ -16,34 +16,20 @@ export const AlertCustom = (props: AlertCustomProps) => {
     setOpenToast(false);
   };
 
-  const [widthWindow, setWidthWindow] = useState(window.innerWidth);
-  const [positionY, setPositionY] = useState<"top" | "bottom">(
-    widthWindow > 500 ? "bottom" : "top"
-  );
-  const [positionX, setPositionX] = useState<"right" | "center">("right");
-  const [widthAlert, setWidthAlert] = useState<string>("40vw");
-
-  useEffect(() => {
-    window.addEventListener("resize", () => {
-      setWidthWindow(window.innerWidth);
-      setPositionY(widthWindow > 670 ? "bottom" : "top");
-      setPositionX(widthWindow > 500 ? "right" : "center");
-      setWidthAlert(widthWindow > 500 ? "40vw" : "80vw");
-    })
-  });
-
   return (
     <Snackbar
       open={openToast}
-      autoHideDuration={6000}
+      autoHideDuration={3000}
+      key={Transition.name}
       onClose={handleClose}
-      anchorOrigin={{ vertical: positionY, horizontal: positionX }}
+      TransitionComponent={Transition}
+      anchorOrigin={{ vertical: "top", horizontal: "center" }}
     >
       <Alert
         severity={variante}
         onClose={handleClose}
-        variant={tema === "light" ? "filled" : "outlined"}
-        sx={{ width: widthAlert, fontWeight: "bold", minWidth: "300px" }}
+        variant={"filled"}
+        sx={{ width: "100%", fontWeight: "bold", minWidth: "300px", maxWidth: "400px" }}
       >
         {msg}
       </Alert>
